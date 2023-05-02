85c562d7ae5b3b2ed73083854eccabc1c6822ea7
AlvaroGuGo303 <alvaro.guillengonzalez@gmail.com>
2023-05-02 19:45:11 +0200

    fixed

--- parcel1Angular/demo/src/app/app-routing.module.ts
+++ parcel1Angular/demo/src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PaymentComponent} from "./payment/payment.component";
 import {SuccessComponent} from "./success/success.component";
+import {APP_BASE_HREF} from "@angular/common";
 
 const routes: Routes = [
-  { path: '', component: PaymentComponent },
-  { path: 'success', component: SuccessComponent }
+  { path: 'app1', component: PaymentComponent },
+  { path: 'app1/success', component: SuccessComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
